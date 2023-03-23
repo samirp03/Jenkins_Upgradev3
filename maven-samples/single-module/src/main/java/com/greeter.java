2e969385b75685d3548d87753ad0b8b028b493dd
samirp03 <101383634+samirp03@users.noreply.github.com>
2023-03-23 17:12:42 +0545

    revert to original

--- maven-samples/single-module/src/main/java/com/greeter.java
+++ maven-samples/single-module/src/main/java/com/greeter.java
@@ -3,15 +3,15 @@ package com.example;
 /**
  * This is a class.
  */
-public class Greeter01 {
+public class Greeter {
 
   /**
    * This is a constructor.
    */
-/*  public Greeter() {
+  public Greeter() {
 
   }
-*/
+
   //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
