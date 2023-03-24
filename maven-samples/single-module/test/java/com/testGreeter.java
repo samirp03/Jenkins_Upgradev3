b1385343211934b67db81d5a9d9f9b69e8e7a849
samirp03 <samir.poudel03@gmail.com>
2023-03-24 11:49:14 +0545

    Adding Test

--- maven-samples/single-module/test/java/com/testGreeter.java
+++ maven-samples/single-module/test/java/com/testGreeter.java
@@ -23,14 +23,14 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
-/*
+
   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-*/
+
     @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
